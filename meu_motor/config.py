b20ad64e66d2d86437c693b841bfc378bb3f92da
arcanjo1995 <francisco.ipad1995@gmail.com>
2026-07-16 10:03:56 -0300

    Create config.py

---
+++ meu_motor/config.py
@@ -0,0 +1,7 @@
+# config.py
+# Constantes e caminhos usados em todo o sistema
+
+NOME_BASE_DEFINITIVA = "resultados_blaze.xlsx"
+VERSAO_CHAVES_HASH = 1
+
+# Outras constantes, se houver, podem ser adicionadas aqui.
