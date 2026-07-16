165120afa30dc2115d64105acef51e24533a1ca9
arcanjo1995 <francisco.ipad1995@gmail.com>
2026-07-16 10:06:15 -0300

    Delete meu_motor directory

--- meu_motor/config.py
+++
@@ -1,7 +0,0 @@
-# config.py
-# Constantes e caminhos usados em todo o sistema
-
-NOME_BASE_DEFINITIVA = "resultados_blaze.xlsx"
-VERSAO_CHAVES_HASH = 1
-
-# Outras constantes, se houver, podem ser adicionadas aqui.
